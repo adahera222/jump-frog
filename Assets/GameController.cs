6f108579e21cc610ac86b2f2014f57dd4491a723
Jon Wood <jon@ninjagiraffes.co.uk>
2013-12-14 22:06:45 +0000

    Almost entirely missed the point
    And that would have been embarrassing.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 	public bool PlayerHasPreviouslyDied() {
-		return false;
 		return PlayerPrefs.HasKey ("PlayerHasPreviouslyDied");
 	}
 
